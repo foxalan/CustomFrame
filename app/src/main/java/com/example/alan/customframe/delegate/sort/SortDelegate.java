affbfbf2091d0df0ec856587ca1a55264e0bdb55
alan <swd-004@mail.foxconn.com>
2018-01-25 08:49:31 +0800

    update sort list

--- app/src/main/java/com/example/alan/customframe/delegate/sort/SortDelegate.java
+++ app/src/main/java/com/example/alan/customframe/delegate/sort/SortDelegate.java
@@ -1,18 +1,21 @@
 package com.example.alan.customframe.delegate.sort;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
 import com.example.alan.customframe.R;
 import com.example.alan.customframe.delegate.home.bottom.BaseBottomItemDelegate;
 
 /**
  * Function :
  * Modify Date : 2018/1/23
- *
  * @Author : Alan
  * Issue : TODO
  * Whether Solve :
  */
 
 public class SortDelegate extends BaseBottomItemDelegate {
+
     @Override
     public Object getLayout() {
         return R.layout.delegate_sort;
@@ -20,6 +23,12 @@ public class SortDelegate extends BaseBottomItemDelegate {
 
     @Override
     public void onBindView() {
+
+    }
+
+    @Override
+    public void onLazyInitView(@Nullable Bundle savedInstanceState) {
+        super.onLazyInitView(savedInstanceState);
 
     }
 }
---
+++ app/src/main/java/com/example/alan/customframe/delegate/sort/list/VerticalListDelegate.java
@@ -0,0 +1,67 @@
+package com.example.alan.customframe.delegate.sort.list;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import com.example.alan.customframe.R;
+import com.example.alan.customframe.delegate.LatteDelegate;
+import com.example.alan.customframe.net.RestClient;
+import com.example.alan.customframe.net.callback.ISuccess;
+
+import butterknife.BindView;
+
+/**
+ * Function :
+ * Modify Date : 2018/1/24
+ *
+ * @Author : Alan
+ * Issue : TODO
+ * Whether Solve :
+ */
+
+public class VerticalListDelegate extends LatteDelegate {
+
+    @BindView(R.id.ryc_sort_vertical)
+    RecyclerView mRecyclerView;
+
+    @Override
+    public Object getLayout() {
+        return R.layout.delegate_sort_vertical_list;
+    }
+
+    @Override
+    public void onBindView() {
+        initRecyclerView();
+    }
+
+    private void initRecyclerView() {
+        final LinearLayoutManager manager = new LinearLayoutManager(getContext());
+        mRecyclerView.setLayoutManager(manager);
+        //屏蔽动画效果
+        mRecyclerView.setItemAnimator(null);
+    }
+
+    @Override
+    public void onLazyInitView(@Nullable Bundle savedInstanceState) {
+        super.onLazyInitView(savedInstanceState);
+
+        RestClient.builder()
+                .url("sort_list.php")
+                .loader(getContext())
+                .success(new ISuccess() {
+                    @Override
+                    public void onSuccess(String response) {
+//                        final List<MultipleItemEntity> data =
+//                                new VerticalListDataConverter().setJsonData(response).convert();
+//                        final SortDelegate delegate = getParentDelegate();
+//                        final SortRecyclerAdapter adapter = new SortRecyclerAdapter(data, delegate);
+//                        mRecyclerView.setAdapter(adapter);
+                    }
+                })
+                .build()
+                .get();
+
+    }
+}
