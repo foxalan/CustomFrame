988280ef0a848bd582fe58c797129e8453acf0c5
alan <swd-004@mail.foxconn.com>
2018-01-31 10:21:45 +0800

    add delegate order commit

---
+++ app/src/main/java/com/example/alan/customframe/delegate/personal/order/OrderCommentDelegate.java
@@ -0,0 +1,25 @@
+package com.example.alan.customframe.delegate.personal.order;
+
+import com.example.alan.customframe.R;
+import com.example.alan.customframe.delegate.LatteDelegate;
+
+/**
+ * Function :
+ * Modify Date : 2018/1/31
+ *
+ * @Author : Alan
+ * Issue : TODO
+ * Whether Solve :
+ */
+
+public class OrderCommentDelegate extends LatteDelegate {
+    @Override
+    public Object getLayout() {
+        return R.layout.delegate_order_comment;
+    }
+
+    @Override
+    public void onBindView() {
+
+    }
+}
